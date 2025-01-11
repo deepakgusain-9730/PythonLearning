042a0e25dbbb0b4545504a5ca52bd1fa2ec25bb5
deepakgusain-9730 <deepak.gusain1310@gmail.com>
2025-01-11 22:08:35 +0530

    Added python code for learning

---
+++ py_assignment/py_assignment/app.py
@@ -0,0 +1,11 @@
+#!flask /bin/python
+from flask import Flask,jsonify,request
+from controller.main import main
+
+app = Flask(__name__)
+app.register_blueprint(main)
+
+
+@app.route('/')
+def home():
+    return 'Welcome To The Api Developed By Deepak Gusain'
